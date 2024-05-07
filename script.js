dc7b609eac188c48099cf8bf52b56a07eac9b5bf
Lea <lee.lea92@gmail.com>
2024-05-07 14:32:04 +0200

    delete div

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 fetch(`https://whatyearisit-backend-two-beta.vercel.app/year`)
   .then((response) => response.json())
   .then((data) => {
-    console.log(data);
     document.querySelector("#year").textContent = data.year;
   });
