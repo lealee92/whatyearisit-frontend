1593096f76aab505ecf2f9e7989118da58439678
Lea <lee.lea92@gmail.com>
2024-05-07 14:26:18 +0200

    dom

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ fetch(`https://whatyearisit-backend-two-beta.vercel.app/year`)
   .then((response) => response.json())
   .then((data) => {
     console.log(data);
-    document.querySelector("#year").textContent = `Year: ${data.year}`;
+    document.querySelector("#year").textContent = data.year;
   });
